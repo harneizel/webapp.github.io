230cb19b379943bd9c541368d31150f4ec71fa08
harneizel <genyievg@gmail.com>
2024-07-13 04:03:32 +0400

    сайтик

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 let tg = window.Telegram.WebApp;
 
+tg.expand();
+
+document.getElementById("name").innerHTML = tg.initDataUnsafe.user.first_name;
+document.getElementById("family").innerHTML =tg.initDataUnsafe.user.last_name;
+
+/*
 fetch('http://147.45.246.252/user_info/${tg.initDataUnsafe.user.id}')
   .then(response => response.json())
   .then(data => {
     document.getElementById("description").innerHTML = data.description;
   })
   .catch(error => console.error('Error:', error));
-
-tg.expand();
-
-document.getElementById("name").innerHTML = tg.initDataUnsafe.user.first_name;
-document.getElementById("family").innerHTML =tg.initDataUnsafe.user.last_name;
-
-
+*/
 
 
 btn.addEventListener('click', function(){
